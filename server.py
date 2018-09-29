bd3c9081b534925a571b6a92e5895e18fb4d5e91
Manny Bhidya <abhidya@vols.utk.edu>
2018-09-28 21:57:52 -0400

    fixed

--- server.py
+++ server.py
@@ -18,13 +18,13 @@ thread_lock = Lock()
 
 def background_thread():
     """Example of how to send server generated events to clients."""
-    count = 0
-    while True:
-        socketio.sleep(10)
-        count += 1
-        socketio.emit('my_response',
-                      {'data': 'Server generated event', 'count': count},
-                      namespace='/test')
+    # count = 0
+    # while True:
+    #     socketio.sleep(10)
+    #     count += 1
+    #     # socketio.emit('my_response',
+    #     #               {'data': 'Server generated event', 'count': count},
+    #     #               namespace='/test')
 
 
 @app.route('/')
